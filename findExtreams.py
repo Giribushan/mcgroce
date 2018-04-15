9d4847843986bf1efb53d1d34943c035c240c8c6
g <giri.amrita92@gmail.com>
2018-04-15 17:33:09 +0530

    Fixes..

--- findExtreams.py
+++ findExtreams.py
@@ -3,7 +3,8 @@ import imutils
 import cv2
 
 def get_extreams(image):
-    print("Entered find extreams method..")
+    #debugging loggers
+    #print("Entered find extreams method..")
     ## load the image nd convert it grey sclae and blur it a bit
     #image = cv2.imread("/home/gch/Desktop/hand.jpg")
     #image = cv2.imread("/home/gch/Desktop/grabcut_out/grabcutout_cv2.png")
@@ -17,12 +18,12 @@ def get_extreams(image):
     # threshold the image, then perform a series of erosions +
     # dilations to remove any small regions of noise
     #thresh = cv2.threshold(gray, 45, 255, cv2.THRESH_BINARY)[1]
-    thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY)[1]
+    thresh = cv2.threshold(gray, 0, 225, cv2.THRESH_BINARY)[1]
     thresh = cv2.erode(thresh, None, iterations=2)
     thresh = cv2.dilate(thresh, None, iterations=2)
     # show the output image
-    cv2.imshow("Applied Erosion and Dilations", thresh)
-    cv2.waitKey(0)
+    #cv2.imshow("Applied Erosion and Dilations", thresh)
+    #cv2.waitKey(0)
 
     # find contours in thresholded image, then grab the largest
     # one
@@ -31,7 +32,7 @@ def get_extreams(image):
     cnts = cnts[0] if imutils.is_cv2() else cnts[1]
     c = max(cnts, key=cv2.contourArea)
 
-    #Before we can find extreme points along a contour, it’s #important to understand that a contour is simply a NumPy #array of (x, y)-coordinates. Therefore, we can leverage #NumPy functions to help us find the extreme coordinates.
+    #Before we can find extreme points along a contour, it is important to understand that a contour is simply a NumPy #array of (x, y)-coordinates. Therefore, we can leverage #NumPy functions to help us find the extreme coordinates.
     
     # determine the most extreme points along the contour
     extLeft = tuple(c[c[:, :, 0].argmin()][0])
@@ -49,24 +50,25 @@ def get_extreams(image):
     cv2.circle(image, extTop, 8, (255, 0, 0), -1)
     cv2.circle(image, extBot, 8, (255, 255, 0), -1)
     ##Show the output image
-    cv2.imshow("img_extreams", image)
-    cv2.waitKey(0)
-    cv2.imwrite("/home/gch/Desktop/temp_out/extreamPts.jpg", image)
+    #cv2.imshow("img_extreams", image)
+    #cv2.waitKey(0)
+    #cv2.imwrite("/home/gch/Desktop/temp_out/extreamPts.jpg", image)
     
     ## draw bbox
     cv2.rectangle(image, (extLeft[0], extTop[1]), (extRight[0], extBot[1]), (255,0,0), 2)
     ##Show the output image
-    cv2.imshow("bbox_image", image)
-    cv2.waitKey(0)
-    cv2.imwrite("/home/gch/Desktop/temp_out/extreamPtsWithBbox.jpg", image)
+    #cv2.imshow("bbox_image", image)
+    #cv2.waitKey(0)
+    #cv2.imwrite("/home/gch/Desktop/temp_out/extreamPtsWithBbox.jpg", image)
     
-    print("image shape _ ", image.shape)
-    print("The extream points are ... ")
-    print("(extLeft, extTop) - ({:d}, {:d})".format(extLeft[0], extTop[1]))
-    print("(extRight, extBot) - ({:d}, {:d})".format(extRight[0], extBot[1]))
-    print("extLeft - ", extLeft)
-    print("extRight - ", extRight)
-    print("extTop - ", extTop)
-    print("extBot - ", extBot)
+    # debugging loggers
+#     print("image shape _ ", image.shape)
+#     print("The extream points are ... ")
+#     print("(extLeft, extTop) - ({:d}, {:d})".format(extLeft[0], extTop[1]))
+#     print("(extRight, extBot) - ({:d}, {:d})".format(extRight[0], extBot[1]))
+#     print("extLeft - ", extLeft)
+#     print("extRight - ", extRight)
+#     print("extTop - ", extTop)
+#     print("extBot - ", extBot)
     return [(extLeft[0], extTop[1]), (extRight[0], extBot[1])]
     
--- grabcut2.py
+++ grabcut2.py
@@ -2,6 +2,11 @@ import numpy as np
 import cv2
 import cv2 as cv
 from matplotlib import pyplot as plt
+import findExtreams as fe
+import os
+import random
+import copy
+#from curses.textpad import rectangle
 
 def addAlpha(img):
     # Adding the alpha channel
@@ -12,206 +17,222 @@ def addAlpha(img):
     img = cv2.merge(rgba,4)
     return img
 
+# bbox coordinates
+bbox_coordinates = []
 # change the name as per the product 
 product_name = "ChocolateCake"
 
-#img = cv2.imread("C:\\Users\\gcheru200\\Pictures\\others\\image4.jpg")
+# Object folder path
+objFolderPath = "C:\\Users\\gcheru200\\Pictures\\chocCake"
+#Background Images folder path
+bgFolderPath = "C:\\Users\\gcheru200\\Pictures\\bg_selected"
 
-#This is the original input image
-img2 = cv2.imread("C:\\Users\\gcheru200\\Pictures\\chocCake\\ChocCake1.png")
-# Perfrom edge detection
-img2WithEdges = cv2.Canny(img2, 0, 255)
-cv2.imwrite("C:\\Users\\gcheru200\\Pictures\\others\\ChocCakeWithEdges.jpg", img2WithEdges)
+#bgFgSetSize is to set many number of diverse (background+Foreground) as you want in the training
+bgFgSetSize = 5
 
-#This image is from edgeDetection
-img = cv2.imread("C:\\Users\\gcheru200\\Pictures\\others\\ChocCakeWithEdges.jpg")
+## variation_size is to specify as many number of variations as you want for a single (Foreground+Background)
+#variation_size = 300
+variation_size = 1
 
-print("Input image shape - ", img.shape)
-#img = cv2.imread("C:\\Users\\gcheru200\\Pictures\\yeppie\\yeppie1.png")
-# Plot the input image
-#imgplot = plt.imshow(cv2.cvtColor(img2, cv2.COLOR_BGR2RGBA))
-imgplot = plt.imshow(img)
-plt.show()
-
-'''
-##replacing the white bg pixels to pure white pixels..
-#r1, g1, b1 = range(165,180), range(165,180), range(169,199) # Original value
-r1, g1, b1 = 0, 0, 0 # Value that we want to replace it with
-r2, g2, b2 = 255,255,255 # Original value
-red, green, blue= img[:,:,0], img[:,:,1], img[:,:,2]
-qmask = (red == r1) & (green == g1) & (blue == b1)
-#mask = (np.any(red >= r1[0]) and np.any(red <= r1[1])) and (np.any(green >= g1[0]) and np.any(green <= g1[1])) and (np.any(blue >= b1[0]) and np.any(blue <= b1[1]))
-#mask = (red >= r1[0]).all() and (red <= r1[1]).all() & ((green >= r2[0]).all() and (green <= r2[1]).all())
-#mask = ((red >= r1[0]).any() and (red <= r1[1]).any()) & ((green >= r2[0]).any() and (green <= r2[1]).any()) & ((blue >= r2[0]).any() and (blue <= r2[1]).any())
-mask = (red in r1).all() & (green in g1).all() & (blue in b1).all()
-img[:,:,:3][mask] = [r2, g2, b2]
-print("The targeted pixels..", img[:,:,:3][mask])
-## after changeing to pure white background..
-imgplot = plt.imshow(img[:,:,:3][mask])
-plt.show()
-'''
-
-mask = np.zeros(img.shape[:2],np.uint8)
-bgdModel = np.zeros((1,65),np.float64)
-fgdModel = np.zeros((1,65),np.float64)
-
-#Initialize the rectangle, 
-# Try the rectangle which will enclose the object. 
-#rect = (34,370,340,700)
-##Apply rectangle up to the size of input image
-
-# Initialize the rectangle to desired area which can minimize the grabcut time..
-#rect = (1, 1, img.shape[1], img.shape[0])
-rect = (490, 222, 804, 470)
-##Apply grab cut..
-cv2.grabCut(img,mask,rect,bgdModel,fgdModel,5,cv2.GC_INIT_WITH_RECT)
-
-mask2 = np.where((mask==2)|(mask==0),0,1).astype('uint8')
-#mask2 = np.where((mask==3),255,0).astype('uint8')
-img = img*mask2[:,:,np.newaxis]
-img2 = img2*mask2[:,:,np.newaxis]
-
-# Adding the alpha channel to canny edge image
-img = addAlpha(img)
-## Adding alpha channel to original image
-img2 = addAlpha(img2)
-cv2.imwrite("/home/gch/Desktop/grabcut_out/grabcutout_cv2.png", img)
-import PIL 
-from PIL import Image
-imgPil = Image.fromarray(img2)
-print("The image pil - ", imgPil)
-#cv2.imshow("GrabCut Output", cv2.cvtColor(img2, cv2.COLOR_BGRA2RGB))
-cv2.imshow("GrabCut Output", img2)
-cv2.waitKey(0)
-print("The alpha channel dimentions..", img[:,:,3].shape)
-
-## Now we are going to get the bbox coordinates as per the mask
-bbox_coordinates = []
-import findExtreams as fe
-bbox_coordinates = fe.get_extreams(img)
-print('bbox_coordinates - ' ,bbox_coordinates)
-
-print("Image dimensions with out extreams - ", img.shape)
-
-## Draw the bbox on top of original image...
-img = img2[bbox_coordinates[0][1]:bbox_coordinates[1][1],bbox_coordinates[0][0]:bbox_coordinates[1][0]]
-print("Image dimensions with extreams - ", img.shape)
-plt.imshow(img)
-plt.show()
-#cv2.waitKey(0)
-
-
-## Now we are going to overlay the object on top of background image..
-#background = cv2.imread("/home/gch/Desktop/storeHD.jpg", cv2.IMREAD_UNCHANGED)
-background = cv2.imread("C:\\Users\\gcheru200\\Pictures\\others\\big-data-binary.png")
-
-alpha = img[:, :, 3]
-import random
-print(random.randint(0,9))
-print("The complete bbox coordinates..")
-print(bbox_coordinates[1][0])
-print(bbox_coordinates[0][0])
-print(bbox_coordinates[1][1])
-print(bbox_coordinates[0][1])
-
-## Calculate the threshold to sample the input object with in the background boundaries
-xlimit = background.shape[0] - img.shape[0]
-ylimit = background.shape[1] - img.shape[1]
-print("The input object width - ", img.shape[0])
-print("The input object height - ", img.shape[1])
-print("Background width - ", background.shape[0])
-print("Background height - ", background.shape[1])
-print("The xlimit value - ", xlimit)
-print("The ylimit value - ", ylimit)
-# The background image used here
-imgplot = plt.imshow(cv2.cvtColor(background, cv2.COLOR_BGR2RGB))
-plt.show()
-
-
-## Now Overlaying the product on top of a background image..
-import random
-import overlay_image as oi
-import matplotlib.pyplot as plt
-import xml.etree.cElementTree as ET
-
-## Specify the dataset_size
-#dataset_size = 300
-dataset_size = 1
-for i in range(dataset_size):
-    ## picking the random coordinates with in the background range..
-    offset_x = random.randint(0,xlimit)
-    offset_y = random.randint(0,ylimit) 
+for j in range(bgFgSetSize):
+    bbox_coordinates = []
+    objImagePath = os.path.join(objFolderPath,random.choice(os.listdir(objFolderPath)))
+    bgImagePath = os.path.join(bgFolderPath,random.choice(os.listdir(bgFolderPath)))
+    #This is the input image with object alone..
+    inputObj = cv2.imread(objImagePath)
+    #cv2.imshow('input image', inputObj)
+    #cv2.waitKey(0)
+    
+    # Perfrom edge detection
+    img = cv2.Canny(inputObj, 100, 255)
+    img = cv2.cvtColor(img, cv2.COLOR_GRAY2RGB)
+    
+    #print("The edges shape - ", img.shape)
+    #imgplot = plt.imshow(img)
+    #plt.show()
+    bbox_coordinates = fe.get_extreams(img)
+    
+    #Using these coordinates to initialize the rectangle for grabcut
+    rect = (bbox_coordinates[0][0], bbox_coordinates[0][1], bbox_coordinates[1][0] - bbox_coordinates[0][0], bbox_coordinates[1][1] - bbox_coordinates[0][1])
+    
+    #print("Input image shape - ", img.shape)
+    #imgplot = plt.imshow(cv2.cvtColor(inputObj, cv2.COLOR_BGR2RGBA))
+    # imgplot = plt.imshow(img)
+    #plt.show()
+    
+    
+    mask = np.zeros(img.shape[:2],np.uint8)
+    bgdModel = np.zeros((1,65),np.float64)
+    fgdModel = np.zeros((1,65),np.float64)
+    #rect = (50,50,450,290)
+    #rect = (1, 1, img.shape[1], img.shape[0])
+    cv.grabCut(inputObj,mask,rect,bgdModel,fgdModel,5,cv.GC_INIT_WITH_RECT)
+    mask2 = np.where((mask==2)|(mask==0),0,1).astype('uint8')
+    img = img*mask2[:,:,np.newaxis]
+    inputObj = inputObj*mask2[:,:,np.newaxis]
+    #cv2.imshow("grabcutOut", inputObj)
+    #cv2.waitKey(0)
+    
+    # Adding the alpha channel to Canny edge image
+    img = addAlpha(img)
+    ## Adding alpha channel to Original image
+    inputObj = addAlpha(inputObj)
+    
+    #cv2.imshow("GrabCut Output", inputObj)
+    #cv2.waitKey(0)
+    #print("The alpha channel dimentions..", img[:,:,3].shape)
+    
+    ## Now we are going to get the bbox coordinates as per the grabcut output
+    bbox_coordinates = fe.get_extreams(img)
     # debugging loggers
-    print("The offset x - ", offset_x)
-    print("The offset y - ", offset_y)
-    background = cv2.imread("C:\\Users\\gcheru200\\Pictures\\others\\big-data-binary.png", cv2.IMREAD_UNCHANGED)
-    # Overlaying the object with background image..Kind of alpha blending..
-    oi.overlay_image_alpha(background,
-                    img[:, :, 0:4],
-                    (offset_x, offset_y),
-                    alpha )
-    cv2.imshow('background blended with foreground', background)
-    cv2.waitKey(0)
-
-    # Global coordinates with respect to background
-#     global_x1 = bbox_coordinates[0][0]  
-#     global_y1 = bbox_coordinates[0][1] 
-#     global_x2 = bbox_coordinates[1][0] + offset_y
-#     global_y2 = bbox_coordinates[1][1] + offset_x
-
-    global_x1 = offset_y
-    global_y1 = offset_x 
-    global_x2 = img.shape[1] + offset_y
-    global_y2 = img.shape[0] + offset_x
+    #print('bbox_coordinates - ' ,bbox_coordinates)
     
+    #print("Image dimensions with out extreams - ", img.shape)
+    ## Cropping the images as per the extream points..
+    inputObj = inputObj[bbox_coordinates[0][1]:bbox_coordinates[1][1],bbox_coordinates[0][0]:bbox_coordinates[1][0]]
+    #img = img[bbox_coordinates[0][1]:bbox_coordinates[1][1],bbox_coordinates[0][0]:bbox_coordinates[1][0]]
+    #print("Image dimensions with extreams - ", img.shape)
     
+    #plt.imshow(inputObj)
+    #plt.show()
+    #cv2.waitKey(0)
+    
+    ## Now we are going to overlay the object on top of background image..
+    #background = cv2.imread("C:\\Users\\gcheru200\\Pictures\\others\\big-data-binary.png")
+    #background = cv2.imread("C:\\Users\\gcheru200\\Pictures\\others\\big-data-binary.png", cv2.IMREAD_UNCHANGED)
+    #print("The background image to be read - ", bgImagePath)
+    background = cv2.imread(bgImagePath, cv2.IMREAD_UNCHANGED)
+    freshBackGround = copy.copy(background)
+    #imgplot = plt.imshow(cv2.cvtColor(background, cv2.COLOR_BGR2RGB))
+    #plt.show()
+    
+    ## Calculate the threshold to sample the input object positions w.r.to background
+    xlimit = background.shape[0] - inputObj.shape[0]
+    ylimit = background.shape[1] - inputObj.shape[1]
     # debugging loggers
-    print("The global coordinates w.r.to background..")
-    print(global_x1,global_y1)
-    print(global_x2,global_y2)
+#     print("The input object width - ", inputObj.shape[0])
+#     print("The input object height - ", inputObj.shape[1])
+#     print("Background width - ", background.shape[0])
+#     print("Background height - ", background.shape[1])
+#     print("The xlimit value - ", xlimit)
+#     print("The ylimit value - ", ylimit)
     
-    ## draw bbox on final image to get a final picture
-    FinalImage_with_bbox = cv2.rectangle(background, (global_x1, global_y1), (global_x2, global_y2), (0,0,255), 2)
-
-    plt.imshow(cv2.cvtColor(FinalImage_with_bbox, cv2.COLOR_BGR2RGB))
-    plt.show()
-    plt.imshow(cv2.cvtColor(background, cv2.COLOR_BGR2RGB))
-    plt.show()
-    ##Show the output image
-    cv2.imshow("FinalImage_with_bbox ", FinalImage_with_bbox )
-    cv2.waitKey(0)
-    cv2.imwrite('/home/gch/Desktop/Final_Images_and_annotations/Images/' + product_name + "_" + str(i) + ".jpg", background)
-    print("Completed saving the training image.." + product_name + "_" + str(i) + ".jpg")
-
-
-    ## Now we are going to parse the bbox coordinates to Pascal VOC annotation format.
-    root = ET.Element("annotation")
-    ET.SubElement(root, "folder").text = "folder"
-    ET.SubElement(root, "filename").text = product_name + "_" + str(i) + ".jpg"
-    ET.SubElement(root, "path").text = "path"
-    source = ET.SubElement(root, "source")
-    ET.SubElement(source, "database").text = "Unknown"
-    size = ET.SubElement(root, "size")
-    ET.SubElement(size, "width").text = str(background.shape[0])
-    ET.SubElement(size, "height").text = str(background.shape[1])
-    ET.SubElement(size, "depth").text = str(3)
-    ET.SubElement(root, "segmented").text = str(0)
-    obj = ET.SubElement(root, "object")
-
-    ET.SubElement(obj, "name").text = product_name 
-    ET.SubElement(obj, "pose").text = "Unspecified"
-    ET.SubElement(obj, "truncated").text = str(0)
-    ET.SubElement(obj, "difficult").text = str(0)
-    bbx = ET.SubElement(obj, "bndbox")
-
-    ET.SubElement(bbx, "xmin").text = str(global_x1)
-    ET.SubElement(bbx, "ymin").text = str(global_y1)
-    ET.SubElement(bbx, "xmax").text = str(global_x2)
-    ET.SubElement(bbx, "ymax").text = str(global_y2)
-
-
-    tree = ET.ElementTree(root)
-    tree.write("C:\\Users\\gcheru200\\Pictures\\others\\Annotaions\\" + product_name + "_" + str(i) + ".xml")
-    # debugging loggers !!!
-    print("Completed saving the annotation.." + product_name + "_" + str(i) + ".xml")
+    
+    
+    ## Now Overlaying the product on top of a background image..
+    import random
+    import overlay_image as oi
+    import matplotlib.pyplot as plt
+    import xml.etree.cElementTree as ET
+    alpha = inputObj[:, :, 3]
+    
+    for i in range(variation_size):
+        ## picking the random coordinates with in the background range..
+        i += j 
+        if variation_size > 1:
+            background = freshBackGround
+        offset_x = random.randint(0,xlimit)
+        offset_y = random.randint(0,ylimit) 
+        # debugging loggers
+        #print("The offset x - ", offset_x)
+        #print("The offset y - ", offset_y)
+        # Overlaying the object with background image..Kind of alpha blending..
+        # So the background image will be manipulated and can be used for training!!!!
+        oi.overlay_image_alpha(background,
+                        inputObj[:, :, 0:4],
+                        (offset_x, offset_y),
+                        alpha )
+        #cv2.imshow('background blended with foreground', background)
+        #cv2.waitKey(0)
+    
+        # Global bbox coordinates with respect to background
+        global_x1 = offset_y
+        global_y1 = offset_x 
+        global_x2 = inputObj.shape[1] + offset_y
+        global_y2 = inputObj.shape[0] + offset_x
+        
+        
+        # debugging loggers
+        #print("The global bbox coordinates w.r.to background..")
+        #print(global_x1,global_y1)
+        #print(global_x2,global_y2)
+        ## draw bbox on final image to get a final picture
+        FinalImage_with_bbox = copy.copy(background)
+        FinalImage_with_bbox = cv2.rectangle(FinalImage_with_bbox, (global_x1, global_y1), (global_x2, global_y2), (0,255,0), 2)
+    
+    #     plt.imshow(cv2.cvtColor(FinalImage_with_bbox, cv2.COLOR_BGR2RGB))
+    #     plt.show()
+    #     plt.imshow(cv2.cvtColor(background, cv2.COLOR_BGR2RGB))
+    #     plt.show()
+        ##Show the output image
+        #cv2.imshow("FinalImage_with_bbox ", FinalImage_with_bbox )
+        #cv2.waitKey(0)
+        cv2.imwrite('C:\\Users\\gcheru200\\Pictures\\others\\Images\\' + product_name + "_fb" + str(j)+ "_v" + str(i) + ".jpg", background)
+        #print("Saved training image as => " + 'C:\\Users\\gcheru200\\Pictures\\others\\Images\\' + product_name + "_" + str(i) + ".jpg")
+    
+        ## Now we are going to parse the bbox coordinates to Pascal VOC annotation format.
+        root = ET.Element("annotation")
+        ET.SubElement(root, "folder").text = "folder"
+        ET.SubElement(root, "filename").text = product_name + "_" + str(i) + ".jpg"
+        ET.SubElement(root, "path").text = "path"
+        source = ET.SubElement(root, "source")
+        ET.SubElement(source, "database").text = "Unknown"
+        size = ET.SubElement(root, "size")
+        ET.SubElement(size, "width").text = str(background.shape[0])
+        ET.SubElement(size, "height").text = str(background.shape[1])
+        ET.SubElement(size, "depth").text = str(3)
+        ET.SubElement(root, "segmented").text = str(0)
+        obj = ET.SubElement(root, "object")
+    
+        ET.SubElement(obj, "name").text = product_name 
+        ET.SubElement(obj, "pose").text = "Unspecified"
+        ET.SubElement(obj, "truncated").text = str(0)
+        ET.SubElement(obj, "difficult").text = str(0)
+        bbx = ET.SubElement(obj, "bndbox")
+    
+        ET.SubElement(bbx, "xmin").text = str(global_x1)
+        ET.SubElement(bbx, "ymin").text = str(global_y1)
+        ET.SubElement(bbx, "xmax").text = str(global_x2)
+        ET.SubElement(bbx, "ymax").text = str(global_y2)
+    
+    
+        tree = ET.ElementTree(root)
+        tree.write("C:\\Users\\gcheru200\\Pictures\\others\\Annotaions\\" + product_name + "_" + "fb" + str(j)+ "_v" + str(i) + ".xml")
+        # debugging loggers !!!
+        #print("Saved annotation as => " + "C:\\Users\\gcheru200\\Pictures\\others\\Annotaions\\" + product_name + "_" + str(i) + ".xml")
+    cv2.destroyAllWindows()
 print("Completed..")
+    
+'''
+#cv2.imwrite("/home/gch/Desktop/grabcut_out/backandwhite_mask.png", img)
+#import scipy.misc
+#print("THe shape of the mask = ", img.shape)
+import PIL
+from PIL import Image
+im = Image.fromarray(img)
+print("The image mode - ", im.mode)
+im = im.convert("RGBA")
+print("The image mode after convertion - ", im.mode)
+im.show()
+im.save("/home/gch/Desktop/grabcut_out/backandwhite_mask.png")
+#scipy.misc.imsave("/home/gch/Desktop/grabcut_out/backandwhite_mask.jpg", img)
+#cv2.imshow('',img)
+#cv2.waitKey(0)
+#plt.imshow(img),plt.colorbar(),plt.show()
+
+'''
+'''
+# newmask is the mask image I manually labelled
+newmask = cv2.imread('newmask.png',0)
+
+# whereever it is marked white (sure foreground), change mask=1
+# whereever it is marked black (sure background), change mask=0
+mask[newmask == 0] = 0
+mask[newmask == 255] = 1
+
+mask, bgdModel, fgdModel = cv2.grabCut(img,mask,None,bgdModel,fgdModel,5,cv2.GC_INIT_WITH_MASK)
+
+mask = np.where((mask==2)|(mask==0),0,1).astype('uint8')
+img = img*mask[:,:,np.newaxis]
+plt.imshow(img),plt.colorbar(),plt.show()
+'''
